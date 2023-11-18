914ae3eee9a5269cd2adfa4f93e1bcecfab9312f
Timen van Gelderen <tvgelderen@outlook.com>
2023-11-18 11:37:05 +0100

    initial setup

---
+++ NetflixCloneAPI/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetflixCloneAPI.Models;
+using NetflixCloneAPI.DTO;
+
+namespace NetflixCloneAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class UserController : Controller
+    {
+        [HttpGet("{id:guid}")]
+        public IActionResult GetUser(Guid id)
+        {
+            return Ok(id);
+        }
+
+        [HttpPut("{id:guid}")]
+        public IActionResult UpdateUser(Guid id)
+        {
+            return Ok(id);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult DeleteUser(Guid id)
+        {
+            return Ok(id);
+        }
+    }
+}
---
+++ NetflixCloneAPI/Data/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetflixCloneAPI.Models;
+
+namespace NetflixCloneAPI.Data;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> option)
+        : base(option)
+    {}
+
+    public DbSet<User> Users { get; set; }
+}
